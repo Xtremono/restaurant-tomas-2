e2b1003802d44877aa98514b601e7c91625d841a
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-06-30 14:44:18 -0400

    turbo stream

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,48 +1,45 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :set_restaurant, only: [:show, :edit, :update]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.order(created_at: :desc)
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
-  end
-
-  def edit
-    @restaurant = Restaurant.find(params[:id])
-  end
-  def update
-    if @restaurant.update(restaurant_params)
-      redirect_to root_path
-    else
-      render :edit, status: :unprocessable_entity
-    end
   end
 
   def new
     @restaurant = Restaurant.new
   end
 
-  def destroy
-    @restaurant.destroy!
-    redirect_to root_path
+  def edit
   end
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    if @restaurant.save!
-      redirect_to restaurant_path(@restaurant)
-    else
-      render new:, status: :unprocessable_entity
+    @restaurant.save
+    respond_to do |format|
+      format.html { redirect_to restaurant_path(@restaurant) }
+      format.turbo_stream
     end
   end
 
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path, notice: "Restaurant deleted successfully."
+  end
+
   private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
-  end
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address)
   end
 end
